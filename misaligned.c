76a6e9d3aa668f5fcc58665c41d72bec6e11cf3a
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-02-04 12:09:34 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -3,49 +3,71 @@
 const int N_MAJ_COLORS = 5;
 const int N_MIN_COLORS = 5;
 
-int Test_printColorMapCount=0;//In test environment
-
 typedef struct 
 {
     char* Maj_Color;
     char* Min_color;
 }ColorPair_set;
 
-int Get_Each_ColorPair_set(void);
+ColorPair_set Get_Each_ColorPair_set(int n);
+int Test_Get_Each_ColorPair_set(int);
+int Test_printColorMap(void);
 
-void printColorMap(int PairN,ColorPair_set ColorPair ) 
+int printColorMap() 
 {
-    printf("%d | %s | %s\n", PairN, ColorPair.Maj_Color, ColorPair.Min_color);
+    ColorPair_set ColorPair[N_MAJ_COLORS*N_MIN_COLORS];
+    
+    int i = 0,j=0;
+    for(i = 0; i < N_MAJ_COLORS; i++) {
+         for(j = 0; j < N_MIN_COLORS; j++) {
+        ColorPair[i * 5 + j] = Get_Each_ColorPair_set(i * 5 + j);
+            printf("%d | %s | %s\n", i * 5 + j, ColorPair[i * 5 + j].Maj_Color, ColorPair[i * 5 + j].Min_color);
+        }
+    }
+    return i*j;
 }
 
 int main() {
     int result;
-    result = Get_Each_ColorPair_set();
+    result = printColorMap();//Test_printColorMap();
     assert(result==25);
     assert(result == N_MAJ_COLORS*N_MIN_COLORS);
+    
+    // check if each time, color pair was received by calling Get_Each_ColorPair_set;
+    // Hence to make sure all required values got printed. Bcz the functionality cannot be determined just by returning i*j
+    assert(result == Test_printColorMap());
+    //printf("%d",Test_printColorMap());
     
    printf("All is well (maybe!)\n");
     return 0;
 }
 
-int Get_Each_ColorPair_set(void)
+ColorPair_set Get_Each_ColorPair_set(int n)
 {
-    int i = 0,j=0;
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     
     ColorPair_set ColorPair;
 
+    ColorPair.Maj_Color = (char*)majorColor[n/N_MAJ_COLORS];
+    ColorPair.Min_color = (char*)minorColor[n%N_MIN_COLORS];
 
-    
+    return ColorPair;
+}
+
+int Test_Get_Each_ColorPair_set(int count)
+{
+   return count++;
+}
+
+int Test_printColorMap()
+{
+    int count=0,r;
+        int i = 0,j=0;
     for(i = 0; i < N_MAJ_COLORS; i++) {
          for(j = 0; j < N_MIN_COLORS; j++) {
-            ColorPair.Maj_Color = (char*)majorColor[(i * 5 + j)/N_MAJ_COLORS];
-            ColorPair.Min_color = (char*)minorColor[j%N_MIN_COLORS];
-          printColorMap( (i * 5 + j),  ColorPair);  
+        r = Test_Get_Each_ColorPair_set(count);
         }
-        
     }
-
-    return i*j;
+    return r;
 }
