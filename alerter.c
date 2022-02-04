73b72e0715d700f405a29c49ce7af25449a3c82d
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-02-04 10:40:01 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,44 +1,48 @@
 #include <stdio.h>
 #include <assert.h>
 
+typedef int (*networkAlert)(float);
+
 int alertFailureCount = 0;
 
-int networkAlertStub(float celcius) {
+int networkAlertStub_RetNotOk(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
+    return 500;
+}
 
+int networkAlertStub_RetOk(float celcius) {
+    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    // Return 200 for ok
+    // Return 500 for not-ok
+    // stub always succeeds and returns 200
     return 200;
 }
 
-void alertInCelcius(float farenheit) {
-     int returnCode=0;
-    //let threshold be okay for celcius -10:40
-    float thresholdup=40, thresholdLow = -10;
-
+void alertInCelcius(float farenheit, networkAlert sendAlert) {
     float celcius = (farenheit - 32) * 5 / 9;
-   
-    if(!(celcius>=thresholdLow && celcius<=thresholdup ))
-    returnCode = networkAlertStub(celcius);
-    
+	int returnCode = 0;
+	
+	returnCode = sendAlert(celcius);
+	
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
+    }
+        else{
+        //ok response
+        //do nothing
     }
 }
 
 int main() {
-    alertInCelcius(32);
-    alertInCelcius(400.5);
-    alertInCelcius(303.6);
-    
-    
-    //alertFailureCount is expected to be 2 ;
-    assert (alertFailureCount == 2);
+	
+    alertInCelcius(400.5, networkAlertStub_RetNotOk);
+    alertInCelcius(303.6, networkAlertStub_RetOk);
+	assert (alertFailureCount == 1);
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
